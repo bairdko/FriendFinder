3401f010b6958c5dba7357e2fb4aa2d482fc280d
bairdko <bairdko@gmail.com>
2018-09-06 21:46:20 -0500

    html done i guess

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+
+
+var friends = [
+  {
+    name: "Mike",
+    photo: "./photos/mike.jpg",
+    score: 10
+  },
+  {
+    name: "Theodore",
+    photo: "./photos/theodore.jpg",
+    score:15
+  },
+  {
+    name: "Sally",
+    photo: "./photos/sally.jpg",
+    score: 20
+  },
+  {
+    name: "Marty",
+    photo: "./photos/marty.jpg",
+    score:25
+  },
+  {
+    name: "Billy Jean",
+    photo: "./photos/billyJean.jpg",
+    score: 30
+  },
+  {
+    name: "Eleanor Louise",
+    photo: "./photos/eleanorLouise.jpg",
+    score: 35
+  },
+  {
+    name: "Chip",
+    photo: "./photos/chip.jpg",
+    score: 40
+  },
+  {
+    name: "Chuck",
+    photo: "./photos/chip.jpg",
+    score: 45
+  },
+  {
+    name: "Astrid",
+    photo: "./photos/astrid.jpg",
+    score: 50
+  }
+];
+
+module.exports = friends;
