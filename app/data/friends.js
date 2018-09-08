1ee8fdf3662016d93a6ba3b6f7ec990ccd0ff944
bairdko <bairdko@gmail.com>
2018-09-08 12:27:21 -0500

    added photos and link to heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,47 +3,47 @@
 var friends = [
   {
     name: "Mike",
-    photo: "./photos/mike.jpg",
+    photo: "https://media.istockphoto.com/photos/always-start-my-day-with-a-fresh-cup-of-coffee-picture-id869773424",
     score: 10
   },
   {
     name: "Theodore",
-    photo: "./photos/theodore.jpg",
+    photo: "https://media.istockphoto.com/photos/portrait-of-a-men-at-the-beach-picture-id997785550",
     score:15
   },
   {
     name: "Sally",
-    photo: "./photos/sally.jpg",
+    photo: "https://media.istockphoto.com/photos/beautiful-woman-enjoying-in-a-sunny-autumn-day-picture-id1007420002",
     score: 20
   },
   {
     name: "Marty",
-    photo: "./photos/marty.jpg",
+    photo: "https://media.istockphoto.com/photos/afro-young-man-using-mobile-phone-and-fixed-gear-bicycle-picture-id890702458",
     score:25
   },
   {
     name: "Billy Jean",
-    photo: "./photos/billyJean.jpg",
+    photo: "https://media.istockphoto.com/photos/preparing-her-favourite-dish-picture-id892674198",
     score: 30
   },
   {
     name: "Eleanor Louise",
-    photo: "./photos/eleanorLouise.jpg",
+    photo: "https://media.istockphoto.com/photos/the-young-emotional-angry-woman-screaming-on-blue-studio-background-picture-id922830250",
     score: 35
   },
   {
     name: "Chip",
-    photo: "./photos/chip.jpg",
+    photo: "https://media.istockphoto.com/photos/portrait-of-worried-bearded-young-man-looking-up-with-hand-on-chin-picture-id930030808",
     score: 40
   },
   {
     name: "Chuck",
-    photo: "./photos/chip.jpg",
+    photo: "https://media.istockphoto.com/photos/skeptic-unsure-uncertain-doubts-concept-young-african-guy-in-formal-picture-id936442344",
     score: 45
   },
   {
     name: "Astrid",
-    photo: "./photos/astrid.jpg",
+    photo: "https://media.istockphoto.com/photos/young-african-woman-relaxing-at-home-using-a-laptop-picture-id921889440",
     score: 50
   }
 ];
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 # ReadMe
 
+#to view on heroku: 
+https://friend-finder-hmwk-kob.herokuapp.com/
+
+
